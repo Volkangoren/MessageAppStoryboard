5b7ad31030d79df8557b6a4867c81ddc0da56636
Volkangoren <volkan_goren52@hotmail.com>
2020-03-12 10:38:43 +0300

    Initial Commit

---
+++ MessageAppStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageAppStoryboard
+//
+//  Created by Volkan on 12.03.2020.
+//  Copyright © 2020 Volkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
